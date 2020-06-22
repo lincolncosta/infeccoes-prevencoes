c6fc66c6e5df0701bea3fccbf0aab5a665a4a6c6
tekpixo <lincoln.mcosta@gmail.com>
2020-06-22 14:42:57 -0300

    formatações finais.

--- analyzer/analyzer.py
+++ analyzer/analyzer.py
@@ -17,14 +17,10 @@ percentActionSquares = actionSquares.percentCalc(
 percentLuckySquares = actionSquares.percentCalc(
     boardSquares, luckySquares)
 
-# comparação de bifurcações
-# zonas de frequência
-# quantidade de cartas de perguntas
-
 print('Considerando as seguintes informações:')
 print('O jogo será jogado com ' + str(diceAmount) +
       ' dado(s) de ' + str(diceSides) + ' lados e em um tabuleiro de ' + str(boardSquares) + ' casas.')
-print('                     ------------------------------')
+print('            ------------------- CASAS DE AÇÃO -------------------')
 print('A média de jogadas para que um dos jogadores vença é aproximadamente ' + str(playsAvg) + '.')
 print('Os jogadores possuem uma porcentagem de aproximadamente ' +
       str(percentActionSquares * 100) + ' de cair em casas de ação (Infecção e Prevenção), ou seja, cada jogador cai em aproximadamente ' + str(percentActionSquares * playsAvg) + ' casas de ação durante uma partida.')
